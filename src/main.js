b40979222d4722f0de87f2e71ebf18179ed6becd
MINT <mint@inslens.com>
2019-01-25 10:30:28 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -116,8 +116,8 @@ Vue.component('user-help', UserHelp)
 Vue.component('notification-center', NotificationCenter)
 Vue.component('user-profile', UserProfile)
 
-axios.defaults.baseURL = 'http://skyhawkapi.huilab.cn/api';
-//axios.defaults.baseURL = 'http://monitor2.huilab.cn/api'
+// axios.defaults.baseURL = 'http://skyhawkapi.huilab.cn/api';
+axios.defaults.baseURL = 'http://monitor2.huilab.cn/api'
 //axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
 
 Vue.prototype.$ajax = axios
