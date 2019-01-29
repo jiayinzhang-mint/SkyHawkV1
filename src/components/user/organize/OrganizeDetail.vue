c929d26277a1ca861fad8be9f0c5d99e11e69bd6
MINT <mint@inslens.com>
2019-01-29 10:03:57 +0800

    active userlist

--- src/components/user/organize/OrganizeDetail.vue
+++ src/components/user/organize/OrganizeDetail.vue
@@ -1,111 +1,124 @@
 <template>
-    <v-container style="height:calc(100vh - 129px);overflow :auto">
-        <v-card v-loading="loading" element-loading-background="rgba(0, 0, 0, 0.3)">
-            <v-toolbar flat color="transparent">
-                <v-toolbar-title style="font-size:17px">{{organizeInfo.name}}</v-toolbar-title>
-            </v-toolbar>
-            <v-data-table :headers="headers" :items="userList" hide-actions no-data-text="暂无数据">
-                <template slot="items" slot-scope="props">
-                    <td class="text-xs-center">{{ props.item.name }}</td>
-                    <td class="text-xs-center">{{ props.item.index }}</td>
-                    <td class="text-xs-center">{{ props.item.phone }}</td>
-                    <td class="text-xs-center">
-                        <v-btn color="primary" round flat @click="showDetail(props.item)">详情</v-btn>
-                    </td>
-
-                </template>
-            </v-data-table>
-            <el-dialog :title="currentUser.name" :visible.sync="userInfoDialog" width="400px">
-                <v-img class="mb-5 elevation-12" style="border-radius:3px" contain :src="currentUser.avatar" lazy-src="/static/assets/lazy.jpg">
-                    <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
-                        <v-progress-linear indeterminate color="grey darken-1" :width="1"></v-progress-linear>
-                    </v-layout>
-                </v-img>
-                <v-list two-line>
-                    <v-list-tile>
-                        <v-list-tile-action>
-                            <v-icon color="primary">contacts</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>{{currentUser.index}}</v-list-tile-title>
-                            <v-list-tile-sub-title>工号</v-list-tile-sub-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </v-list>
-            </el-dialog>
-
-        </v-card>
-    </v-container>
+  <v-container style="height:calc(100vh - 129px);overflow :auto">
+    <v-card v-loading="loading" element-loading-background="rgba(0, 0, 0, 0.3)">
+      <v-toolbar flat color="transparent">
+        <v-toolbar-title style="font-size:17px">{{organizeInfo.name}}</v-toolbar-title>
+      </v-toolbar>
+      <v-data-table :headers="headers" :items="userList" hide-actions no-data-text="暂无数据">
+        <template slot="items" slot-scope="props">
+          <td class="text-xs-center">{{ props.item.name }}</td>
+          <td class="text-xs-center">{{ props.item.index }}</td>
+          <td class="text-xs-center">{{ props.item.phone }}</td>
+          <td class="text-xs-center">
+            <v-chip v-if="props.item.wechat" small text-color="white" color="green">在线</v-chip>
+            <v-chip v-else small text-color="white" color="red darken-3">离线</v-chip>
+          </td>
+          <td class="text-xs-center">
+            <v-btn color="primary" round flat @click="showDetail(props.item)">详情</v-btn>
+          </td>
+        </template>
+      </v-data-table>
+      <el-dialog :title="currentUser.name" :visible.sync="userInfoDialog" width="400px">
+        <v-img
+          class="mb-5 elevation-12"
+          style="border-radius:3px"
+          contain
+          :src="currentUser.avatar"
+          lazy-src="/static/assets/lazy.jpg"
+        >
+          <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+            <v-progress-linear indeterminate color="grey darken-1" :width="1"></v-progress-linear>
+          </v-layout>
+        </v-img>
+        <v-list two-line>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon color="primary">contacts</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{currentUser.index}}</v-list-tile-title>
+              <v-list-tile-sub-title>工号</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </el-dialog>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
-    data: () => ({
-        organizeInfo: [],
-        userList: [],
-        currentUser: [],
-        loading: true,
-        userInfoDialog: false,
-        headers: [
-            {
-                text: "姓名",
-                align: "center",
-                sortable: false,
-                value: "name"
-            },
-            {
-                text: "工号",
-                value: "index",
-                align: "center",
-                sortable: false
-            },
-            {
-                text: "移动电话",
-                value: "phone",
-                align: "center",
-                sortable: false
-            },
-            {
-                text: "操作",
-                align: "center",
-                sortable: false
-            }
-        ]
-    }),
-    methods: {
-        getOrganizeInfo() {
-            this.loading = true;
-            this.$ajax
-                .get("/organize/detail", {
-                    params: {
-                        token: this.userInfo.token,
-                        id: this.$route.params.id
-                    }
-                })
-                .then(data => {
-                    data = data.data;
-                    this.organizeInfo = data.organizeInfo;
-                    this.userList = data.userList;
-                    this.loading = false;
-                    // console.log(this.userList);
-                });
-        },
-        showDetail(item) {
-            this.userInfoDialog = true;
-            this.currentUser = item;
-        }
+  data: () => ({
+    organizeInfo: [],
+    userList: [],
+    currentUser: [],
+    loading: true,
+    userInfoDialog: false,
+    headers: [
+      {
+        text: "姓名",
+        align: "center",
+        sortable: false,
+        value: "name"
+      },
+      {
+        text: "工号",
+        value: "index",
+        align: "center",
+        sortable: false
+      },
+      {
+        text: "移动电话",
+        value: "phone",
+        align: "center",
+        sortable: false
+      },
+      {
+        text: "微信",
+        align: "center",
+        sortable: false
+      },
+      {
+        text: "操作",
+        align: "center",
+        sortable: false
+      }
+    ]
+  }),
+  methods: {
+    getOrganizeInfo() {
+      this.loading = true;
+      this.$ajax
+        .get("/organize/detail", {
+          params: {
+            token: this.userInfo.token,
+            id: this.$route.params.id
+          }
+        })
+        .then(data => {
+          data = data.data;
+          this.organizeInfo = data.organizeInfo;
+          this.userList = data.userList;
+          this.loading = false;
+          // console.log(this.userList);
+        });
     },
-    mounted() {
-        this.getOrganizeInfo();
-    },
-    computed: {
-        ...mapGetters(["userInfo"])
-    },
-    beforeRouteUpdate(to, from, next) {
-        next();
-        this.getOrganizeInfo();
+    showDetail(item) {
+      this.userInfoDialog = true;
+      this.currentUser = item;
     }
+  },
+  mounted() {
+    this.getOrganizeInfo();
+  },
+  computed: {
+    ...mapGetters(["userInfo"])
+  },
+  beforeRouteUpdate(to, from, next) {
+    next();
+    this.getOrganizeInfo();
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,17 +1,49 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-import colors from 'vuetify/es5/util/colors'
-import qs from 'qs';
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import axios from "axios";
+import colors from "vuetify/es5/util/colors";
+import qs from "qs";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 import {
-    Vuetify,
-    VApp, // required
+  Vuetify,
+  VApp, // required
+  VNavigationDrawer,
+  VToolbar,
+  VList,
+  VDivider,
+  VCard,
+  VTabs,
+  VBtn,
+  VTextField,
+  VIcon,
+  VSubheader,
+  VMenu,
+  VAvatar,
+  VDataTable,
+  VDataIterator,
+  VSelect,
+  VBottomNav,
+  VImg,
+  VProgressLinear,
+  VGrid,
+  VFooter,
+  VChip,
+  transitions,
+  VDialog,
+  VTimeline,
+  VHover,
+  VParallax
+} from "vuetify";
+import { Ripple } from "vuetify/lib/directives";
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
     VNavigationDrawer,
     VToolbar,
     VList,
@@ -38,109 +70,75 @@ import {
     VTimeline,
     VHover,
     VParallax
-} from 'vuetify'
-import { Ripple } from 'vuetify/lib/directives'
+  },
+  directives: {
+    Ripple
+  },
+  theme: {
+    primary: colors.blue.lighten2
+  }
+});
+require("vuetify/src/stylus/app.styl");
 
-Vue.use(Vuetify, {
-    components: {
-        VApp,
-        VNavigationDrawer,
-        VToolbar,
-        VList,
-        VDivider,
-        VCard,
-        VTabs,
-        VBtn,
-        VTextField,
-        VIcon,
-        VSubheader,
-        VMenu,
-        VAvatar,
-        VDataTable,
-        VDataIterator,
-        VSelect,
-        VBottomNav,
-        VImg,
-        VProgressLinear,
-        VGrid,
-        VFooter,
-        VChip,
-        transitions,
-        VDialog,
-        VTimeline,
-        VHover,
-        VParallax
-    },
-    directives: {
-        Ripple
-    },
-    theme: {
-        primary: colors.blue.lighten2,
-    }
-
-})
-require('vuetify/src/stylus/app.styl')
-
-
-if (process.env.NODE_ENV === 'development') {
-    require('element-ui/lib/theme-chalk/index.css')
+if (process.env.NODE_ENV === "development") {
+  require("element-ui/lib/theme-chalk/index.css");
 }
 
-import ELEMENT from 'element-ui'
-Vue.use(ELEMENT)
+import ELEMENT from "element-ui";
+Vue.use(ELEMENT);
 
-
-import VueInitialsImg from 'vue-initials-img';
+import VueInitialsImg from "vue-initials-img";
 import "viewerjs/dist/viewer.css";
 import Viewer from "v-viewer";
 
 Vue.use(Viewer, {
-    defaultOptions: {
-        title: false,
-        toolbar: 0,
-        tooltip: false,
-        navbar: 0
-    }
-})
+  defaultOptions: {
+    title: false,
+    toolbar: 0,
+    tooltip: false,
+    navbar: 0
+  }
+});
 
 Vue.use(VueInitialsImg);
-Vue.use(require('vue-moment'));
+Vue.use(require("vue-moment"));
 
+import SettingCenter from "@/components/sidebar/SettingCenter";
+import UserHelp from "@/components/sidebar/UserHelp";
+import NotificationCenter from "@/components/sidebar/NotificationCenter";
+import UserProfile from "@/components/sidebar/UserProfile";
+Vue.component("setting-center", SettingCenter);
+Vue.component("user-help", UserHelp);
+Vue.component("notification-center", NotificationCenter);
+Vue.component("user-profile", UserProfile);
 
-import SettingCenter from '@/components/sidebar/SettingCenter'
-import UserHelp from '@/components/sidebar/UserHelp'
-import NotificationCenter from '@/components/sidebar/NotificationCenter'
-import UserProfile from '@/components/sidebar/UserProfile'
-Vue.component('setting-center', SettingCenter)
-Vue.component('user-help', UserHelp)
-Vue.component('notification-center', NotificationCenter)
-Vue.component('user-profile', UserProfile)
-
-// axios.defaults.baseURL = 'http://skyhawkapi.huilab.cn/api';
+axios.defaults.baseURL = "http://skyhawkapi.huilab.cn/api";
 //axios.defaults.baseURL = 'http://monitor2.huilab.cn/api'
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+//axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
 
-Vue.prototype.$ajax = axios
+Vue.prototype.$ajax = axios;
 
-import store from './store/store'
+import store from "./store/store";
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    store,
-    components: { App },
-    template: '<App/>'
-})
-
-axios.interceptors.request.use((config) => {
-    //在发送请求之前做某件事
-    if (config.method === 'post' || config.method === 'put') {
-        config.data = qs.stringify(config.data);
-    }
-    return config;
-}, (error) => {
-    _.toast("错误的传参", 'fail');
-    return Promise.reject(error);
+  el: "#app",
+  router,
+  store,
+  components: { App },
+  template: "<App/>"
 });
 
+axios.interceptors.request.use(
+  config => {
+    //在发送请求之前做某件事
+    if (config.method === "post" || config.method === "put") {
+      config.data = qs.stringify(config.data);
+    }
+    return config;
+  },
+  error => {
+    _.toast("错误的传参", "fail");
+    return Promise.reject(error);
+  }
+);
